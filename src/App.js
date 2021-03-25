b083b9484b26ca6869fdc00e78604cc34a5c6fff
Yoon Kim <78077021+yoonthecoder@users.noreply.github.com>
2021-03-24 20:37:43 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import {useState, useMemo} from 'react';
+import uuid from 'react-uuid';
+import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
+
+const newNote = () => {
+ return { id: uuid(),
+  title:"",
+  body: "",
+  lastModified: Date.now()}
+};
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [filterednotes, setFilteredNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(false);
+  
+
+  // Add a new note
+  const onAddNote = () => {
+    
+
+    // update the list of notes
+    setNotes([newNote(), ...notes]);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === activeNote) {
+        return updatedNote;
+      }
+      return note;
+    });
+    
+    setNotes(updatedNotesArray);
+  }
+
+  const onDeleteNote = (idToDelete) => {
+     setNotes(notes.filter((note) => note.id !== idToDelete))
+  }
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  }
+
+  return (
+    <div className="App">
+     <Sidebar notes={notes} setNotes={setNotes} onAddNote={onAddNote} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote}  filterednotes={filterednotes} setFilteredNotes={setFilteredNotes}/>
+     <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  )
+}
+
+export default App;
